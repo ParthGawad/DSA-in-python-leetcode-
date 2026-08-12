76334de8ebd0f8153ec36cc9457ac90ea9351e70
Parth Gawad <163501890+ParthGawad@users.noreply.github.com>
2026-08-12 21:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.43%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,9 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        
-        for num in nums:
-            # Add 'num' to every subset created so far
-            res += [curr + [num] for curr in res]
+        result = [] 
+        subset = []
+        nums_len = len(nums)
+
+        # backtracking function
+        def dfs(i) :
+            # base case
+            if i >= nums_len :  
+                # copying all the values in the subset to the result
+                result.append(subset.copy())
+                return 
             
-        return res
+            # including values into the subsets that we want to include & perform dfs from on
+            subset.append(nums[i])
+            dfs(i+1)
+
+            # excluding values from the subsets by popping it that we don't want to include & perform dfs from on
+            subset.pop()
+            dfs(i+1)
+
+        # start of the backtracking dfs
+        dfs(0)
+        return result             
